e8c2b2b439934197b22cdd9ddacb737f47cc6c82
SGDiomov18 <SGDiomov18@codingburgas.bg>
2022-10-18 15:38:16 +0300

    Initial Commit

--- src/CD.java
+++
@@ -1,8 +0,0 @@
-public class CD implements MediaPlayer{
-
-
-    @Override
-    public void play() {
-        System.out.println("Play some metal");
-    }
-}
--- src/DVD.java
+++
@@ -1,8 +0,0 @@
-public class DVD implements MediaPlayer{
-
-
-    @Override
-    public void play() {
-        System.out.println("Play some more metal");
-    }
-}
